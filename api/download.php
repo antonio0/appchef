93a9322d18e74ccfc2364b9d14c50e68b94ba070
Antonio Marino <antonio.marino@cern.ch>
2014-11-14 00:32:40 -0800

    stringing things together and solving tricky system shit

--- api/download.php
+++ api/download.php
@@ -4,13 +4,32 @@
 $appname = $_GET["appname"];
 $author = $_GET["author"];
 
-$userpath = 'user-'.$author;
+$userpath = '/Applications/XAMPP/htdocs/projects/user-'.$author;
 $apppath = $userpath.'/app-'.$appname;
+exec("cd $apppath && zip -r app.zip $appname");
 
-exec("zip -r $apppath/app.zip $apppath/$appname");
-
-header("Content-disposition: attachment; filename=$apppath/app.zip");
-header('Content-type: application/zip');
-readfile("$apppath/app.zip");
-
+//header("Content-disposition: attachment; filename=app.zip");
+//header('Content-type: application/zip');
+//readfile("app.zip");
+chdir($apppath);
+$file = "app.zip";
+if (headers_sent()) {
+    echo 'HTTP header already sent';
+} else {
+    if (!is_file($file)) {
+        header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+        echo 'File not found';
+    } else if (!is_readable($file)) {
+        header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+        echo 'File not readable';
+    } else {
+        header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+        header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: Binary");
+        header("Content-Length: ".filesize($file));
+        header("Content-Disposition: attachment; filename=\"".basename($file)."\"");
+        readfile($file);
+        exit;
+    }
+}
 ?>
