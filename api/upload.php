3ee2630c3e2fb106bf50fdb68a34c97f1eb9610a
Antonio Marino <antonio.marino@cern.ch>
2014-11-13 18:22:25 -0800

    API for json uploading and proj folder download

---
+++ api/upload.php
@@ -0,0 +1,27 @@
+<?php
+
+// app metadata from get params
+$appname = $_GET["appname"];
+$author = $_GET["author"];
+
+// json description of the app as post
+$json = $_POST("jsonapp");
+
+$userpath = "user-$author";
+if (!file_exists($userpath))
+  mkdir($userpath, 0777, true);
+
+$apppath = "$userpath/app-$appname";
+if (!file_exists($apppath)) {
+    mkdir($apppath, 0777, true);
+}
+
+$jsonfile = fopen("app.json","w");
+echo fwrite($jsonfile, $json);
+fclose($jsonfile);
+
+echo exec("./jsonTest $jsonfile $apppath/jessies.txt");
+
+exec("python test.php")
+
+?>
